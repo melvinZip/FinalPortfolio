919c4b5dff98d6b018bae9ca6e076fcbe03ba74d
melvinZip <149271956+melvinZip@users.noreply.github.com>
2024-01-22 19:34:34 +0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Melvin Daynata</span> currently studying in Sibugay Technical Institute Incorporated (STII), in Bachelor of Science in Information Technology (BSIT)! <br><br> I like solving problems, also I do sports like basketball, Volleyball and I played Online Games. Well most of my time I spend on coding to enhance my coding skills."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
